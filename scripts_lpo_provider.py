ace54254466fbe64e7d5a25c2d40e4f4f7ead0d4
eguilley <elsa.guilley@lpo.fr>
2020-04-20 13:59:22 +0200

    Initialisation

---
+++ scripts_lpo_provider.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+
+"""
+/***************************************************************************
+        ScriptsLPO : scripts_lpo_provider.py
+        -------------------
+        Date                 : 2020-04-16
+        Copyright            : (C) 2020 by Elsa Guilley (LPO AuRA)
+        Email                : lpo-aura@lpo.fr
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+__author__ = 'Elsa Guilley (LPO AuRA)'
+__date__ = '2020-04-16'
+__copyright__ = '(C) 2020 by Elsa Guilley (LPO AuRA)'
+
+# This will get replaced with a git SHA1 when you do a git archive
+__revision__ = '$Format:%H$'
+
+import os
+from qgis.PyQt.QtGui import QIcon
+
+from qgis.core import QgsProcessingProvider
+from .extract_data import ExtractData
+
+pluginPath = os.path.dirname(__file__)
+
+
+class ScriptsLPOProvider(QgsProcessingProvider):
+
+    def __init__(self):
+        """
+        Default constructor.
+        """
+        QgsProcessingProvider.__init__(self)
+
+    def id(self):
+        """
+        Returns the unique provider id, used for identifying the provider. This
+        string should be a unique, short, character only string, eg "qgis" or
+        "gdal". This string should not be localised.
+        """
+        return 'scriptsLPO'
+
+    def name(self):
+        """
+        Returns the provider name, which is used to describe the provider
+        within the GUI.
+
+        This string should be short (e.g. "Lastools") and localised.
+        """
+        return self.tr('Processing scripts LPO')
+
+    def icon(self):
+        """
+        Should return a QIcon which is used for your provider inside
+        the Processing toolbox.
+        """
+        return QIcon(os.path.join(pluginPath, 'icons', 'logo_LPO.png'))
+
+    def unload(self):
+        """
+        Unloads the provider. Any tear-down steps required by the provider
+        should be implemented here.
+        """
+        pass
+
+    def loadAlgorithms(self):
+        """
+        Loads all algorithms belonging to this provider.
+        """
+        algorithms = [ExtractData()]
+        for algo in algorithms:
+            self.addAlgorithm(algo)
+
+    #def longName(self):
+        """
+        Returns the a longer version of the provider name, which can include
+        extra details such as version numbers. E.g. "Lastools LIDAR tools
+        (version 2.2.1)". This string should be localised. The default
+        implementation returns the same string as name().
+        """
+        #return self.name()
