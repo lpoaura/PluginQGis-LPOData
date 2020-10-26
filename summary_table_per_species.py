65be3de9357b5dfa47194f82248b9d6665cab559
eguilley <elsa.guilley@lpo.fr>
2020-10-26 16:15:05 +0100

    Ultimes corrections

--- summary_table_per_species.py
+++ summary_table_per_species.py
@@ -434,8 +434,8 @@ class SummaryTablePerSpecies(QgsProcessingAlgorithm):
                         LEFT JOIN taxonomie.taxref t ON obs.taxref_cdnom = t.cd_nom
                         LEFT JOIN taxonomie.bib_taxref_rangs r ON t.id_rang = r.id_rang
                         LEFT JOIN communes com ON obs.id_synthese = com.id_synthese
-                        LEFT JOIN taxonomie.vm_statut_lr lr ON (obs.taxref_cdnom/*, obs.source_id_sp*/) = (lr.cd_nom/*, lr.vn_id*/)
-                        LEFT JOIN taxonomie.vm_statut_protection p ON (obs.taxref_cdnom/*, obs.source_id_sp*/) = (p.cd_nom/*, p.vn_id*/)
+                        LEFT JOIN taxonomie.vm_statut_lr lr ON (obs.taxref_cdnom, obs.nom_sci) = (lr.cd_nom, lr.vn_nom_sci)
+                        LEFT JOIN taxonomie.vm_statut_protection p ON (obs.taxref_cdnom, obs.nom_sci) = (p.cd_nom, p.vn_nom_sci)
                         GROUP BY
                         /*obs.source_id_sp
                         , */obs.taxref_cdnom
