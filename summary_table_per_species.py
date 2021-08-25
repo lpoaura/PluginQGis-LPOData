10e31d79b10b4ba07e34ae0168eb409b54ec8957
Julien GIRARD-CLAUDON <51160496+lpojgc@users.noreply.github.com>
2021-08-25 08:36:19 +0200

    Update summary_table_per_species.py

--- summary_table_per_species.py
+++ summary_table_per_species.py
@@ -525,7 +525,7 @@ class SummaryTablePerSpecies(QgsProcessingAlgorithm):
         # Add action to layer
         with open(os.path.join(pluginPath, 'format_csv.py'), 'r') as file:
             action_code = file.read()
-        action = QgsAction(QgsAction.GenericPython, 'Exporter la couche sous format Excel dans mon dossier utilisateur avec la mise en forme adaptée', action_code, os.path.join(pluginPath, 'icons', 'excel.png'), False, 'Exporter sous format Excel dans mon dossier utilisateur avec la mise en forme adaptée', {'Layer'})
+        action = QgsAction(QgsAction.GenericPython, 'Exporter la couche sous format Excel', action_code, os.path.join(pluginPath, 'icons', 'excel.png'), False, 'Exporter sous format Excel dans mon dossier utilisateur avec la mise en forme adaptée', {'Layer'})
         layer_summary.actions().addAction(action)
         # JOKE
         with open(os.path.join(pluginPath, 'joke.py'), 'r') as file:
