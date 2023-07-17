de871ed5809918a39092816839b4757bd027c3bd
afourneyron <85483402+afourneyron@users.noreply.github.com>
2023-07-17 15:24:45 +0200

    change select source data depuis VM

--- config/mv_source.sql
+++ config/mv_source.sql
@@ -1,6 +1,8 @@
+CREATE MATERIALIZED VIEW dbadmin.mv_source as          
+select 'list_source' as rang, array_agg(distinct split_part(t_sources.desc_source, ' '::text, 1)) AS list_source 
+from gn_synthese.t_sources
+where t_sources.desc_source ilike '%[%]%'
+group by 1 ;
 
-CREATE materialized VIEW dbadmin.mv_source AS (
-select split_part(desc_source,' ',1) source, array_agg(id_source) 
-FROM gn_synthese.t_sources
-group by split_part(desc_source,' ',1));
-create index on dbadmin.mv_source(source);
+-- View indexes:
+CREATE INDEX mv_source_source_idx ON dbadmin.mv_source USING btree (list_source);
