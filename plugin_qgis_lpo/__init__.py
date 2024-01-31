87898cff8d3a7b8d5bcf6d02fdce49168814efde
Fred C <42280769+hypsug0@users.noreply.github.com>
2024-01-31 09:41:30 +0100

    debug start

--- plugin_qgis_lpo/__init__.py
+++ plugin_qgis_lpo/__init__.py
@@ -2,18 +2,18 @@ import os
 
 from qgis.gui import QgisInterface
 
-from plugin_qgis_lpo.qgis_plugin_tools.infrastructure.debugging import (  # noqa F401
-    setup_debugpy,
-    setup_ptvsd,
-    setup_pydevd,
-)
+# from plugin_qgis_lpo.qgis_plugin_tools.infrastructure.debugging import (  # noqa F401
+#     setup_debugpy,
+#     setup_ptvsd,
+#     setup_pydevd,
+# )
 
-debugger = os.environ.get("QGIS_PLUGIN_USE_DEBUGGER", "").lower()
-if debugger in {"debugpy", "ptvsd", "pydevd"}:
-    locals()["setup_" + debugger]()
+# debugger = os.environ.get("QGIS_PLUGIN_USE_DEBUGGER", "").lower()
+# if debugger in {"debugpy", "ptvsd", "pydevd"}:
+#     locals()["setup_" + debugger]()
 
 
 def classFactory(iface: QgisInterface):  # noqa N802
-    from plugin_qgis_lpo.plugin import Plugin
+    from .plugin import Plugin
 
     return Plugin(iface)
