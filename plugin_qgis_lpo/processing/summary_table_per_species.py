c1781852907dbf048f6bac4bc0eb1e4c5809e595
Fred C <42280769+hypsug0@users.noreply.github.com>
2024-02-01 10:01:36 +0100

    set AuRA LR status as default

--- plugin_qgis_lpo/processing/summary_table_per_species.py
+++ plugin_qgis_lpo/processing/summary_table_per_species.py
@@ -122,6 +122,7 @@ QGIS (couche de type polygones).
                         , SUM(CASE WHEN mortalite THEN 1 ELSE 0 END)    AS nb_mortalite
                         , st.lr_france
                         , st.lr_ra
+                        , st.lr_auv
                         , st.n2k
                         , st.prot_nat as protection_nat
                         , st.conv_berne
@@ -144,6 +145,7 @@ QGIS (couche de type polygones).
                         , r.nom_rang
                         , st.lr_france
                         , st.lr_ra
+                        , st.lr_auv
                         , st.n2k
                         , st.prot_nat
                         , st.conv_berne
@@ -162,8 +164,9 @@ QGIS (couche de type polygones).
                         , nb_dates                                          AS "Nb de dates"
                         , nb_mortalite                                      AS "Nb de données de mortalité"
                         , lr_france                                         AS "LR France"
-                        , lr_ra                                             AS "LR régionale"
-                        , n2k                                           AS "Natura 2000"
+                        , lr_ra                                             AS "LR Rhône-Alpes"
+                        , lr_auv                                            AS "LR Auvergne"
+                        , n2k                                               AS "Natura 2000"
                         , protection_nat                                    AS "Protection nationale"
                         , conv_berne                                        AS "Convention de Berne"
                         , conv_bonn                                         AS "Convention de Bonn"
