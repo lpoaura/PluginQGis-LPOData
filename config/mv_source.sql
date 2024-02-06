26af544efa8bfb4805a09e4d6aac2362f48a41a8
Fred C <42280769+hypsug0@users.noreply.github.com>
2024-02-06 23:06:42 +0100

    WIP actions

--- config/mv_source.sql
+++ config/mv_source.sql
@@ -1,5 +1,5 @@
-CREATE MATERIALIZED VIEW dbadmin.mv_source as          
-select 'list_source' as rang, array_agg(distinct split_part(t_sources.desc_source, ' '::text, 1)) AS list_source 
+CREATE MATERIALIZED VIEW dbadmin.mv_source as
+select 'list_source' as rang, array_agg(distinct split_part(t_sources.desc_source, ' '::text, 1)) AS list_source
 from gn_synthese.t_sources
 where t_sources.desc_source ilike '%[%]%'
 group by 1 ;
