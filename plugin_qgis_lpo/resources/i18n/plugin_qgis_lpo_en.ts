5e206faca43a3b0e34b3d1dc7993e5002caba836
Fred C <42280769+hypsug0@users.noreply.github.com>
2024-03-07 14:03:47 +0100

    first translation file

---
+++ plugin_qgis_lpo/resources/i18n/plugin_qgis_lpo_en.ts
@@ -0,0 +1,75 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>QgisLpoPlugin</name>
+    <message>
+        <location filename="../../plugin_main.py" line="79"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugin_main.py" line="88"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugin_main.py" line="199"/>
+        <source>Everything ran OK.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugin_main.py" line="205"/>
+        <source>Houston, we&apos;ve got a problem: {}</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>wdg_plugin_qgis_lpo_settings</name>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="14"/>
+        <source>Traitement des données LPO - Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="44"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;PluginTitle - Version X.X.X&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="75"/>
+        <source>Miscellaneous</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="124"/>
+        <source>Report an issue</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="146"/>
+        <source>Version used to save settings:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="168"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="190"/>
+        <source>Reset setttings to factory defaults</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="209"/>
+        <source>Enable debug mode.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="218"/>
+        <source>Debug mode (degraded performances)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
