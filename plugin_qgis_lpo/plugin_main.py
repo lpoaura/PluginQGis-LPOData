1e5b22478fcfda6dcc668b839b73d8418ee3ed2c
Fred C <fcloitre@dbwildlife.info>
2024-03-18 10:10:52 +0100

    WIP set background tasks

--- plugin_qgis_lpo/plugin_main.py
+++ plugin_qgis_lpo/plugin_main.py
@@ -32,6 +32,7 @@ from plugin_qgis_lpo.__about__ import (
     __title__,
     __uri_homepage__,
 )
+from plugin_qgis_lpo.commons.tasking_refresh_data import bg_task
 from plugin_qgis_lpo.gui.dlg_settings import PlgOptionsFactory
 from plugin_qgis_lpo.gui.menu_tools import MenuTools
 from plugin_qgis_lpo.processing.provider import QgisLpoProvider
@@ -63,6 +64,7 @@ class QgisLpoPlugin:
         self.iface: QgisInterface = iface
         self.tools_menu: Optional[QMenu] = None
         self.main_menu: Optional[QMenu] = None
+        self.tm = QgsApplication.taskManager()
         # translation
         # initialize the locale
         self.locale: str = QgsSettings().value("locale/userLocale", QLocale().name())[
@@ -169,8 +171,14 @@ class QgisLpoPlugin:
                 "Attention",
                 "La carte par espèces est accessible via la barre d'outils d'Extensions",
             )
-
-        self.populateSettings()
+        # -- Post UI initialization
+        self.log(
+            message="Plugin QGIS LpO loaded",
+            log_level=0,
+            push=True,
+            duration=60,
+        )
+        self.iface.initializationCompleted.connect(self.populateSettings)
 
     def populateSettings(self):
         try:
@@ -184,8 +192,10 @@ class QgisLpoPlugin:
                     push=True,
                     duration=60,
                 )
-                processing.run(
-                    "plugin_qgis_lpo:RefreshData", {"DATABASE": "geonature_lpo"}
+                self.tm.addTask(
+                    bg_task(
+                        "plugin_qgis_lpo:RefreshData", {"DATABASE": "geonature_lpo"}
+                    )
                 )
                 self.log(
                     message=f"Loading GeoNature terminated",
@@ -202,6 +212,7 @@ class QgisLpoPlugin:
             raise QgsProcessingException(
                 f"Could not retrieve connection details : {str(exc)}"
             ) from exc  # processing.run("plugin_qgis_lpo:RefreshData", {"DATABASE": "geonature_lpo"})
+        return
 
     def runEspeces(self):  # noqa N802
         connection_name = get_connection_name()
@@ -256,6 +267,7 @@ class QgisLpoPlugin:
         :raises Exception: if there is no item in the feed
         """
         try:
+            self.populateSettings()
             self.log(
                 message=self.tr("Everything ran OK."),
                 log_level=3,
