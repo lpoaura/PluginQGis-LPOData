4232bf6dbe15d1a568b532115920d883274ce150
Julien GIRARD-CLAUDON <51160496+lpojgc@users.noreply.github.com>
2025-09-18 07:54:12 +0200

    Update t_parameters.sql

--- config/t_parameters.sql
+++ config/t_parameters.sql
@@ -13,7 +13,7 @@ INSERT INTO gn_commons.t_parameters (
 VALUES (
        0, 
        'plugin_qgis_lpo_status_columns',
-       'Liste des colonnes de statuts de protection/conservation à utilisées pour le plugin QGIS LPO',
+       'Liste des colonnes de statuts de protection/conservation à utilisées pour le plugin QGIS LPO'
        , '"{''lr_france'':''LR France'',''lr_r'': ''LR Régionale'',''n2k'':''Natura 2000'',''prot_nat'':''Protection nationale'',''conv_berne'':''Convention de Berne'',''conv_bonn'':''Convention de Bonn''}"'
        , NULL);
 
